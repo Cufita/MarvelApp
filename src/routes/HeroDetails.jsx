e35b959c763b761b4580fc9992c215c83299bbc1
Cufita <facuamores@gmail.com>
2023-07-27 14:38:28 -0300

    Hero Details finished

--- src/routes/HeroDetails.jsx
+++ src/routes/HeroDetails.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+//Import hooks
+import { useParams } from "react-router-dom";
+
+//Import utils
+
+import { fetchHero } from "../utils/utils";
 export default function HeroDetails() {
-  return <div>HeroDetails</div>;
+  let { id } = useParams();
+
+  const [hero, setHero] = useState();
+
+  useEffect(() => {
+    fetchHero(id)
+      .then((data) => setHero(data[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!hero) return;
+  return (
+    <div className="container large">
+      <div className="hero_details-container">
+        <img
+          src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+          alt="hero image"
+        />
+        <div className="hero_details">
+          <h3>Name</h3>
+          <p>{hero.name}</p>
+          {hero.description ? (
+            <>
+              <h4>Description</h4>
+              <p>{hero.description}</p>
+            </>
+          ) : null}
+          <div className="hero_series">
+            <h4>Series</h4>
+            <ul>
+              {hero.series.items.map((series) => (
+                <li key={Math.random * 1000}>{series.name}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
